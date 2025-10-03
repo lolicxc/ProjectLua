0c1071e180917bc0de06d19bf4da373ce208ce1e
lolicxc <doloreescaparrozz@hotmail.com>
2025-10-03 10:58:15 -0300

    main

---
+++ main.lua
@@ -0,0 +1,7 @@
+
+function love.draw()
+    love.graphics.print("Hola", 400, 300)
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.circle("fill", 100, 100, 50, 5) -- Draw white circle with 100 segments.
+end
+
